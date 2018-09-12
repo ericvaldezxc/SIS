107c4459be85a50dafb30918af2a8e9a67b59666
ericvaldezxc <ericvaldezxc24@yahoo.com>
2018-09-13 01:52:31 +0800

    pray

--- src/connection/DBConfiguration.java
+++ src/connection/DBConfiguration.java
@@ -8,7 +8,7 @@ public class DBConfiguration {
 	public Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sis_13", "root", "");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sis_17", "root", "");
 			return conn;
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
